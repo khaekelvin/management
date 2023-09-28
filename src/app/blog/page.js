9ad6600060256dd9a1279556960fbafe2a872a0e
KING <kelvinksd500@gmail.com>
2023-09-28 19:31:51 +0100

    blog

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -5,16 +5,6 @@ const posts = [
     href: "#",
     description:
       " With integrated communication tools, SchoolFlow ensures that information flows smoothly between administrators, teachers, students, and parents. Instant notifications, announcements, and messaging keep everyone connected and informed.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
   },
   // More posts...
   {
@@ -23,16 +13,6 @@ const posts = [
     href: "#",
     description:
       "SchoolFlow simplifies complex administrative processes such as student enrollment, attendance tracking, scheduling, and report generation. This frees up valuable time for educators to focus on teaching and students to focus on learning.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
   },
   {
     id: 3,
@@ -40,16 +20,6 @@ const posts = [
     href: "#",
     description:
       "School administrators gain valuable insights through data analytics, helping them make informed decisions about curriculum enhancements, resource allocation, and strategic planning.",
-    date: "Mar 16, 2020",
-    datetime: "2020-03-16",
-    category: { title: "Marketing", href: "#" },
-    author: {
-      name: "Michael Foster",
-      role: "Co-Founder / CTO",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
   },
 ];
 
@@ -62,52 +32,26 @@ export default function Blog() {
             What is SchoolFLow?
           </h2>
           <p className='mt-2 text-lg leading-8 text-gray-600'>
-            Learn how to grow your business with our expert advice.
+            Manage your data effectively.
           </p>
         </div>
         <div className='mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3'>
           {posts.map((post) => (
             <article
               key={post.id}
-              className='flex max-w-xl flex-col items-start justify-between'
+              className='max-w-xl flex-col items-start justify-between'
             >
-              <div className='flex items-center gap-x-4 text-xs'>
-                <time dateTime={post.datetime} className='text-gray-500'>
-                  {post.date}
-                </time>
-                <a
-                  href={post.category.href}
-                  className='relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100'
-                >
-                  {post.category.title}
-                </a>
-              </div>
+              <div className='flex items-center gap-x-4 text-xs'></div>
               <div className='group relative'>
-                <h3 className='mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600'>
+                <h3 className='mt-3 text-lg font-semibold leading-9 text-gray-900 group-hover:text-gray-600'>
                   <a href={post.href}>
                     <span className='absolute inset-0' />
                     {post.title}
                   </a>
                 </h3>
-                <p className='mt-5 line-clamp-3 text-sm leading-6 text-gray-600'>
+                <p className='mt-5  text-sm leading-6 text-gray-600'>
                   {post.description}
                 </p>
-              </div>
-              <div className='relative mt-8 flex items-center gap-x-4'>
-                <img
-                  src={post.author.imageUrl}
-                  alt=''
-                  className='h-10 w-10 rounded-full bg-gray-50'
-                />
-                <div className='text-sm leading-6'>
-                  <p className='font-semibold text-gray-900'>
-                    <a href={post.author.href}>
-                      <span className='absolute inset-0' />
-                      {post.author.name}
-                    </a>
-                  </p>
-                  <p className='text-gray-600'>{post.author.role}</p>
-                </div>
               </div>
             </article>
           ))}
