cd9f768a70c582cb4685d3b1e0a38049035e6a87
KING <kelvinksd500@gmail.com>
2023-09-28 17:11:25 +0100

    flex

--- src/app/features/features.js
+++ src/app/features/features.js
@@ -8,21 +8,21 @@ const features = [
   {
     name: "Push to deploy.",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.",
+      "The Student Information Management feature of our school management system ensures that all student-related data is centralized, organized, and easily accessible to authorized users.",
     icon: CloudArrowUpIcon,
   },
   {
     name: "SSL certificates.",
     description:
-      "Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.",
+      "This feature serves as the foundation for various administrative and academic processes, enabling streamlined operations and enhanced communication.",
     icon: LockClosedIcon,
   },
-  {
-    name: "Database backups.",
-    description:
-      "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
-    icon: ServerIcon,
-  },
+  //   {
+  //     name: "Database backups.",
+  //     description:
+  //       "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+  //     icon: ServerIcon,
+  //   },
 ];
 
 export default function Features() {
@@ -32,17 +32,17 @@ export default function Features() {
         <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
           <div className='lg:pr-8 lg:pt-4'>
             <div className='lg:max-w-lg'>
-              <h2 className='text-base font-semibold leading-7 text-indigo-600'>
+              {/* <h2 className='text-base font-semibold leading-7 text-indigo-600'>
                 Deploy faster
-              </h2>
+              </h2> */}
               <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                A better workflow
+                Student Information Management
               </p>
-              <p className='mt-6 text-lg leading-8 text-gray-600'>
+              {/* <p className='mt-6 text-lg leading-8 text-gray-600'>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
                 Maiores impedit perferendis suscipit eaque, iste dolor
                 cupiditate blanditiis ratione.
-              </p>
+              </p> */}
               <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
                 {features.map((feature) => (
                   <div key={feature.name} className='relative pl-9'>
@@ -66,6 +66,47 @@ export default function Features() {
             width={2432}
             height={1442}
           />
+        </div>
+      </div>
+
+      <div className='mx-auto max-w-7xl px-6 lg:px-8'>
+        <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
+          <div className='lg:pr-8 lg:pt-4 flex'>
+            <img
+              src='https://tailwindui.com/img/component-images/dark-project-app-screenshot.png'
+              alt='Product screenshot'
+              className='w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0'
+              width={2432}
+              height={1442}
+            />
+            <div className='lg:max-w-lg'>
+              {/* <h2 className='text-base font-semibold leading-7 text-indigo-600'>
+                Deploy faster
+              </h2> */}
+              <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+                Student Information Management
+              </p>
+              {/* <p className='mt-6 text-lg leading-8 text-gray-600'>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Maiores impedit perferendis suscipit eaque, iste dolor
+                cupiditate blanditiis ratione.
+              </p> */}
+              <dl className='mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none'>
+                {features.map((feature) => (
+                  <div key={feature.name} className='relative pl-9'>
+                    <dt className='inline font-semibold text-gray-900'>
+                      <feature.icon
+                        className='absolute left-1 top-1 h-5 w-5 text-indigo-600'
+                        aria-hidden='true'
+                      />
+                      {feature.name}
+                    </dt>{" "}
+                    <dd className='inline'>{feature.description}</dd>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
         </div>
       </div>
     </div>
