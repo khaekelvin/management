69bbde5793f0ecc160c659ce18a77c26eb89ff88
KING <kelvinksd500@gmail.com>
2023-09-28 16:39:57 +0100

    feat

--- src/app/features/features.js
+++ src/app/features/features.js
@@ -25,7 +25,7 @@ const features = [
   },
 ];
 
-export default function Feature() {
+export default function Features() {
   return (
     <div className='overflow-hidden bg-white py-24 sm:py-32'>
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
