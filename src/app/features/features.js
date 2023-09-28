9c11753882ac20ce8a266f9f318b997fac829e16
KING <kelvinksd500@gmail.com>
2023-09-28 17:32:33 +0100

    all

--- src/app/features/features.js
+++ src/app/features/features.js
@@ -25,6 +25,27 @@ const features = [
   //   },
 ];
 
+const featureses = [
+  {
+    name: "Push to deploy.",
+    description:
+      "The Student Information Management feature of our school management system ensures that all student-related data is centralized, organized, and easily accessible to authorized users.",
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: "SSL certificates.",
+    description:
+      "This feature serves as the foundation for various administrative and academic processes, enabling streamlined operations and enhanced communication.",
+    icon: LockClosedIcon,
+  },
+  //   {
+  //     name: "Database backups.",
+  //     description:
+  //       "Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.",
+  //     icon: ServerIcon,
+  //   },
+];
+
 export default function Features() {
   return (
     <div className='overflow-hidden bg-white py-24 sm:py-32'>
@@ -59,32 +80,32 @@ export default function Features() {
               </dl>
             </div>
           </div>
-          <img
+          {/* <img
             src='https://tailwindui.com/img/component-images/dark-project-app-screenshot.png'
             alt='Product screenshot'
             className='w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0'
             width={2432}
             height={1442}
-          />
+          /> */}
         </div>
       </div>
 
       <div className='mx-auto max-w-7xl px-6 lg:px-8'>
         <div className='mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2'>
           <div className='lg:pr-8 lg:pt-4 flex'>
-            <img
+            {/* <img
               src='https://tailwindui.com/img/component-images/dark-project-app-screenshot.png'
               alt='Product screenshot'
               className='w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0'
               width={2432}
               height={1442}
-            />
+            /> */}
             <div className='lg:max-w-lg'>
               {/* <h2 className='text-base font-semibold leading-7 text-indigo-600'>
                 Deploy faster
               </h2> */}
               <p className='mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
-                Student Information Management
+                Grade and Progress Tracking
               </p>
               {/* <p className='mt-6 text-lg leading-8 text-gray-600'>
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit.
