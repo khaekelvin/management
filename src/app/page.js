efbb0d9d6bf31f5bd087e1d3340ad6f1add8f031
KING <kelvinksd500@gmail.com>
2023-09-28 16:03:31 +0100

    files

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Head from "./head/page";
+
+export default function Home() {
+  return (
+    <main className='flex overflow-x-hidden min-h-screen bg-white w-screen flex-col'>
+      {/* <HeaderBrand /> */}
+      <Head />
+    </main>
+  );
+}
