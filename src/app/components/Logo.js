f9360dc846724ef6b24decfe37c4dff7886c6fc5
KING <kelvinksd500@gmail.com>
2023-10-02 12:40:04 +0100

    size

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -4,7 +4,7 @@ import { FaHubspot } from "react-icons/fa";
 function Logo() {
   return (
     <h1 className='flex'>
-      <FaHubspot className='h-8 w-auto to-blue-600' />
+      <FaHubspot className='h-10 w-auto text-amber-300' />
       {/* School<span className='text-amber-400 font-bold font-sans'>Flow</span> */}
     </h1>
   );
