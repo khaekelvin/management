27a1488006c248dc802f084f284d05489451016a
KING <kelvinksd500@gmail.com>
2023-10-02 12:35:38 +0100

    logog

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { FaHubspot } from "react-icons/fa";
 
 function Logo() {
   return (
-    <h1>
-      School<span className='text-amber-400 '>Flow</span>
+    <h1 className='flex'>
+      <FaHubspot className='h-8 w-auto to-blue-600' />
+      {/* School<span className='text-amber-400 font-bold font-sans'>Flow</span> */}
     </h1>
   );
 }
