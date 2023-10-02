96b31f08333d9c68d0f5d363f7db6b9097183d8e
KING <kelvinksd500@gmail.com>
2023-10-02 14:36:24 +0100

    changed logo color

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -4,8 +4,9 @@ import { FaHubspot } from "react-icons/fa";
 function Logo() {
   return (
     <h1 className='flex'>
-      <FaHubspot className='h-10 w-auto text-amber-300' />
+      <FaHubspot className='h-10 w-auto text-blue-600' />
       {/* School<span className='text-amber-400 font-bold font-sans'>Flow</span> */}
+      {/* text-amber-300 */}
     </h1>
   );
 }
