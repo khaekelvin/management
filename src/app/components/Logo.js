bf11e53f0afe8473a028c3bf09fa0a0a81abe928
KING <kelvinksd500@gmail.com>
2023-10-02 12:13:06 +0100

    adding text logo

---
+++ src/app/components/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <h1>
+      School<span className='text-amber-400 '>Flow</span>
+    </h1>
+  );
+}
+
+export default Logo;
--- src/app/head/page.js
+++ src/app/head/page.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Logo from "../components/Logo";
 
 const navigation = [
   { name: "Home", href: "/" },
@@ -23,11 +24,12 @@ export default function Head() {
           <div className='flex lg:flex-1'>
             <a href='#' className='-m-1.5 p-1.5'>
               <span className='sr-only'>Your Company</span>
-              <img
+              {/* <img
                 className='h-8 w-auto'
                 src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
                 alt=''
-              />
+              /> */}
+              <Logo />
             </a>
           </div>
           <div className='flex lg:hidden'>
