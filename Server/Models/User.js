5be7ba923f969d622f86fa3ebe66d963a12574c0
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-05-23 16:26:50 +0100

    Remove unused files and dependencies

--- Server/Models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-    name:String,
-    email:String,
-    paswword:String,
-});
-
-module.exports = mongoose.model("users",userSchema);
---
+++ src/app/layout.js
@@ -0,0 +1,46 @@
+"use client"
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({ children }) {
+  const redirectUri = typeof window !== 'undefined' ? window.location.origin + '/dashboard' : '';
+  
+  return (
+    <html lang="en">
+      <Auth0Provider
+        domain="dev-p63jl88r8hn0qngy.us.auth0.com"
+        clientId="qJuocD169mnwQRwYrZ7Ty8oDkNZWLoJo"
+        authorizationParams={{
+          redirect_uri: redirectUri
+        }}
+      >
+        <body className={inter.className + " overflow-x-hidden"}>{children}</body>
+      </Auth0Provider>
+    </html>
+  );
+}
+
+// "use client"
+// import { useEffect } from 'react';
+// import { Auth0Provider } from '@auth0/auth0-react';
+
+// export default function RootLayout({ children }) {
+//   useEffect(() => {
+//     const redirectUri = window.location.origin + '/dashboard';
+//     // Use the redirectUri as needed
+//   }, []);
+
+//   return (
+//     <html lang="en">
+//       <Auth0Provider
+//         domain="dev-p63jl88r8hn0qngy.us.auth0.com"
+//         clientId="qJuocD169mnwQRwYrZ7Ty8oDkNZWLoJo"
+//       >
+//         <body>{children}</body>
+//       </Auth0Provider>
+//     </html>
+//   );
+// }
